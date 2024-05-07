5728353cdc1dc9419b55151d88867f76ecd6705e
JackieOuzman <jackie.ouzman@csiro.au>
2024-05-07 15:05:36 +0930

    should be done :0

--- Pasture assessments.R
+++ Pasture assessments.R
@@ -16,7 +16,7 @@ LP_biomass_post_trail <- read.csv("W:/VF/2024/pasture assessments/LP_biomass_gri
 
 Pinnaroo_2021_biomass_post_trail <- read.csv("W:/VF/2024/pasture assessments/Pinnaroo_2021/Pin2021_Bimass_post_from_dist__join.csv")
 
-
+Pinnaroo_2022_biomass_post_trail <- read.csv("W:/VF/2024/pasture assessments/Pinnaroo_2022/Pin2022_Biomass_post_frm_LAI_join.csv")
 
 ################################################################################
 ## tidy up df
@@ -41,6 +41,11 @@ Pinnaroo_2021_biomass_post_trail <-
 str(Pinnaroo_2021_biomass_post_trail)
 
 #2022 Pinnaroo
+str(Pinnaroo_2022_biomass_post_trail)
+Pinnaroo_2022_biomass_post_trail <- Pinnaroo_2022_biomass_post_trail %>% select(FID, Name,  grid_code) %>% 
+  rename(biomass = grid_code,
+         VF_name = Name)
+str(Pinnaroo_2022_biomass_post_trail)
 
 #################################################################################
 
@@ -118,3 +123,41 @@ Summary_Pinnaroo_2021_biomass_post_trail <- Pinnaroo_2021_biomass_post_trail %>%
             Biomass_SE =    Biomass_SD / sqrt(n()))
 
 Summary_Pinnaroo_2021_biomass_post_trail
+
+
+
+#2022 Pinnaroo
+
+str(Pinnaroo_2022_biomass_post_trail)
+
+ANOVA_Pinnaroo_2022_biomass_post_trail <- aov(biomass ~ VF_name , data = Pinnaroo_2022_biomass_post_trail)
+
+summary(ANOVA_Pinnaroo_2022_biomass_post_trail)
+TukeyHSD(ANOVA_Pinnaroo_2022_biomass_post_trail)
+
+## P value = 2e-16 The p-value is greater than the significance level 0.1 (or 0.05, 0.01, 0.001)
+#we can conclude that the biomass in the subpaddocks ARE significant different.
+#Tukey HSD indicates that  subpaddocks are different from one another
+#But I think this is because we have soooo many obervation point!
+
+### do we have equal variance?
+leveneTest(biomass ~ VF_name , data = Pinnaroo_2022_biomass_post_trail)
+
+#From the output above we can see that the p-value IS less than the significance level of 0.05. 
+#This means that there IS evidence to suggest that the variance across groups is statistically significantly different. 
+#Therefore, we can't assume the homogeneity of variances in the different treatment groups.
+# not equal variance
+
+# Non-parametric alternative to one-way ANOVA test
+
+kruskal.test(biomass ~ VF_name , data = Pinnaroo_2022_biomass_post_trail)
+
+# Averages
+Summary_Pinnaroo_2022_biomass_post_trail <- Pinnaroo_2022_biomass_post_trail %>%
+  group_by(VF_name)%>%
+  summarise(mean_Biomass =  mean(biomass, na.rm = TRUE),
+            Biomass_SD =    sd(biomass, na.rm = TRUE),
+            Biomass_SE =    Biomass_SD / sqrt(n()))
+
+Summary_Pinnaroo_2022_biomass_post_trail
+
